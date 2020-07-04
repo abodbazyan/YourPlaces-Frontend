bf26f274eafd00ead87994e5b03d0fbeaa2dac86
abdelfattah-bazyan <abod_bazyan646@hotmail.com>
2020-07-04 11:08:43 +0300

    Fixed property name in USERS

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h1>Users Page</h1>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'John Doe',
+      image: 'https://source.unsplash.com/random/160x160?person',
+      places: '3',
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
