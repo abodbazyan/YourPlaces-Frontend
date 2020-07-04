2b37610943ebf9eacbad353ce23767b95779f630
abdelfattah-bazyan <abod_bazyan646@hotmail.com>
2020-07-04 10:19:55 +0300

    App initialized with basic setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <h1>Let's start!</h1>
+    </Router>
+  );
+}
+
+export default App;
