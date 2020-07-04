0120807a552d6761955b6188d268f40f6e943305
abdelfattah-bazyan <abod_bazyan646@hotmail.com>
2020-07-04 13:32:18 +0300

    Added UserPlaces page to the routing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Users} />
           <Route exact path="/places/new" component={NewPlace} />
+          <Route exact path="/:userId/places" component={UserPlaces} />
           <Redirect to="/" />
         </Switch>
       </main>
