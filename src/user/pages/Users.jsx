29247cb8048fcc8b53502c3c8c41330c5ce4dc5a
abdelfattah-bazyan <abod_bazyan646@hotmail.com>
2020-07-13 01:47:59 +0300

    Implementing useHttpClient hook

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -3,40 +3,31 @@ import React, { useEffect, useState, Fragment } from 'react';
 import UsersList from '../components/UsersList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUser, setLoadedUser] = useState();
 
   useEffect(() => {
     const fetchUsers = async () => {
-      setIsLoading(true);
       try {
-        const response = await fetch('http://localhost:8000/api/users');
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
+        const responseData = await sendRequest(
+          'http://localhost:8000/api/users'
+        );
 
         setLoadedUser(responseData.users);
       } catch (err) {
-        setError(err.message);
+        // NOTE: Handled in the useHttpClient hook
       }
-      setIsLoading(false);
     };
 
     fetchUsers();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
+  }, [sendRequest]);
 
   return (
     <Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
